689e8ec6036bb33bf3feac19496cd7611cfe3283
Jerry Hu <jerryhu1@live.nl>
2016-12-04 16:29:12 +0100

    test test json working on website

--- app/test/test.js
+++ app/test/test.js
@@ -6,17 +6,33 @@ angular.module('gameApp.test', ['ngRoute'])
 
     .controller('TestCtrl', ['$scope', 'TestService',  function($scope, TestService) {
 
-        $scope.categories = ["Adjectives and adverbs", "Articles", "Imperative"];
-
+        $scope.categories = ["Adjectives or adverb", "Articles", "Imperative"];
+        $scope.tests = [];
 
         $scope.getAllTests = function()
         {
             TestService.getAllTests().success(function(result){
 
-                console.log(result);
-                $scope.tests = result;
+                angular.forEach(result, function(){
+
+                   $scope.tests.push(result);
+                })
+            });
+        };
+
+        $scope.getTestsByCategory = function(category)
+        {
+            TestService.getAllTests().success(function(result){
+
+                angular.forEach(result, function(item){
+                    console.log(item.category);
+                    console.log(category);
+                    if(item.category == category) {
+
+                        $scope.tests.push(item);
+                    }
+                });
             });
         }
-
 
     }]);
